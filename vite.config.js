686afe09dbb510fe16cdff12bb119b8ec284033f
John Matthew Lubi <138768200+winraaawr@users.noreply.github.com>
2023-07-29 11:39:43 +0800

    Update vite.config.js (deployment)

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import Icons from 'unplugin-icons/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/anifox/',
   plugins: [
     Icons({ compiler: 'vue3' }),
     vue(),
