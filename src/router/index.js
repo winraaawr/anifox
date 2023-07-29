2197b1155dd9c962c9fcf1df1741c20790f0bbf7
John Matthew Lubi <138768200+winraaawr@users.noreply.github.com>
2023-07-29 21:33:11 +0800

    attempt bug fix version 1.0.1
    404 Anime Page not found bug fix attempt

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import ViewAnime from '../views/ViewAnime.vue'
 import About from '../views/About.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
